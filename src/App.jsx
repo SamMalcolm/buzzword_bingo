c177e1a29e7cfd61945a97e16fe9e2fcdc733bb4
SamMalcolm <sam_malcolm@live.com.au>
2020-06-24 11:44:13 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+	return (
+		<div>
+			<h1>BUZZWORD BINGO!</h1>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.querySelector(".react_container"));
